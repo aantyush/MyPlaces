3229b2ad1cd4f341ae2a6c66cb1fbb35cd4f2202
Anton Antyushin <antushinn@mail.ru>
2020-07-09 00:01:51 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Anton Antyushin on 08.07.2020.
+//  Copyright © 2020 Anton Antyushin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var LocationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
